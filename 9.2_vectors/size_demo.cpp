c696262fe9c129df73043fc440fde6d4fd6a9602
sush1236paul <paulsushant003@gmail.com>
2026-06-11 07:11:27 +0000

    Create size_demo.cpp

---
+++ 9.2_vectors/size_demo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> numbers = {10,20,30};
+
+    std::cout << "Size: "
+              << numbers.size()
+              << std::endl;
+
+    return 0;
+}
